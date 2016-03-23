2253d2dc14e54d8c2e114b0c076a80aaedc4e38a
Pennetta-Contractor, Thomas (CORP) <Thomas.Pennetta-Contractor@adp.com>
2016-03-23 14:31:29 -0400

    Had to remove tracking of typings directory

--- typings/browser.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference path="browser/ambient/chai/index.d.ts" />
-/// <reference path="browser/ambient/form-data/index.d.ts" />
-/// <reference path="browser/ambient/mocha/index.d.ts" />
-/// <reference path="browser/ambient/node/index.d.ts" />
-/// <reference path="browser/ambient/request/index.d.ts" />
--- typings/main.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference path="main/ambient/chai/index.d.ts" />
-/// <reference path="main/ambient/form-data/index.d.ts" />
-/// <reference path="main/ambient/mocha/index.d.ts" />
-/// <reference path="main/ambient/node/index.d.ts" />
-/// <reference path="main/ambient/request/index.d.ts" />
